eb6accc37cc5aea21121c30ee901ff58c77d052a
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-12-19 12:44:05 +0600

    login2 added

--- src/components/Login/Login2.jsx
+++ src/components/Login/Login2.jsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import app from '../../firebase/firebase.config';
 
 const Login2 = () => {
+    const [user, setUser] = useState(null);
+    const [success, setSuccess] = useState("")
+    const [error, setError] = useState("")
+    const auth = getAuth(app)
+    console.log(user)
+
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const from = event.target;
+        const email = from.email.value;
+        const password = from.password.value;
+
+        setError("");
+        setSuccess("")
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError("Please add at least one upperCase")
+            return;
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                // Signed in 
+                const loggedUser = result.user;
+                setUser(loggedUser)
+                setSuccess("User login successfully")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(errorMessage)
+            });
+    }
     return (
-        <div className='text-center'>
-            <h3>This is login</h3>
+        <div className=''>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card mt-5">
+                            <div className="card-header">
+                                <h3>Login</h3>
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className="form-label">
+                                            Email address
+                                        </label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password" className="form-label">
+                                            Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            required
+                                        />
+                                    </div>
+                                    <p className='text-danger'>{error}</p>
+                                    <p className='text-success'>{success}</p>
+                                    <button type="submit" className="btn btn-primary">
+                                        Login
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
