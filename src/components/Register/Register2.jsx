183c46c7539629c24985a5d74668ec81509949fa
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-12-19 10:56:21 +0600

    Register2 functionality complete

--- src/components/Register/Register2.jsx
+++ src/components/Register/Register2.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
 const Register2 = () => {
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+    const auth = getAuth(app)
     const handleEmailChange = (e) => {
         console.log(e.target.value)
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        setError("")
+        setSuccess("")
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password)
+        // firebase auth
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setSuccess("User successfully created")
+                setError("")
+            })
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+            })
     }
     return (
         <div className='text-center'>
             <h3>Please Register</h3>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your email' />
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your email' required />
                 <br />
-                <input type="password" name="password" id="password" placeholder='Your password' />
+                <input type="password" name="password" id="password" placeholder='Your password' required />
                 <br />
-                <button>Register</button>
+                <input type="submit" value="Register" />
             </form>
+            <h5 className='text-danger'>{error}</h5>
+            <h5 className='text-success'>{success}</h5>
         </div>
     );
 };
